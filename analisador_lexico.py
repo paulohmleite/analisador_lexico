7b3f2541a4f9b08e9c49fdc8821a2799455cb561
paulohmleite <paulohmleite1@gmail.com>
2022-09-18 00:29:38 -0400

    subindo analisador léxico
    subindo analisador léxico

---
+++ analisador_lexico.py
@@ -0,0 +1,35 @@
+"""
+Analisador léxico
+
+Implementado por:
+Paulo Henrique M. Leite 
+RGA: 201719050287
+
+"""
+
+# bibliotecas utilizadas
+import nltk
+import re
+
+
+nltk.download('punkt')
+
+file = open('programa_simples.c', 'r')
+process = file.read()
+
+# funções 
+def limpar_comentarios(process):
+  # remover comentarios do tipo //
+  comentarios_uma_linha = re.sub("//.*", "", process)
+
+  # remover comentarios do tipo /* */
+  comentarios_multiplas_linhas = re.sub("/\*[^*]*\*+(?:[^/*][^*]*\*+)*/", "", comentarios_uma_linha)
+  print(comentarios_multiplas_linhas)
+
+  # retornando codigo sem comentarios
+  return comentarios_multiplas_linhas
+
+
+
+if __name__ == '__main__':
+    codigo_sem_comentarios = limpar_comentarios(process)
---
+++ programa_simples.c
@@ -0,0 +1,21 @@
+// bibliotecas
+#include <stdio.h>
+
+
+// main
+int main()
+{
+     int a,a1=0;
+     int b ;
+
+     a = 10;
+     a = a1 + 1;
+     if(a<=10) 
+     {
+        b=a++ ;
+     }
+
+     printf(a) ;
+
+     return 0;
+}
